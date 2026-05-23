f458188dc18e9486fa03b10a54552b3bbcfad052
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-05-23 19:08:25 +0530

    Create uvm_tb_top.sv
    Initial template of uvm_tb_top. This contains the design instantiation, interface declaration and other things

---
+++ tb/uvm_tb_top.sv
@@ -0,0 +1,130 @@
+module uvm_tb_top;
+  
+  bit clk;
+  bit rst_n;
+  genvar i;  
+  
+  axi_xbar_if axi_xif(.clock(clk), .rst_n(rst_n));
+  
+
+  axilxbar axi_crossbar_uut (
+     .S_AXI_ACLK    (clk),
+     .S_AXI_ARESETN (rst_n),
+
+     // Slave AXI-Lite Write Address Channel
+     .S_AXI_AWVALID (axi_xif.s_axi_awvalid),
+     .S_AXI_AWREADY (axi_xif.s_axi_awready),
+     .S_AXI_AWADDR  (axi_xif.s_axi_awaddr),
+     .S_AXI_AWPROT  (axi_xif.s_axi_awprot),
+
+     // Write Data
+     .S_AXI_WVALID  (axi_xif.s_axi_wvalid),
+     .S_AXI_WREADY  (axi_xif.s_axi_wready),
+     .S_AXI_WDATA   (axi_xif.s_axi_wdata),
+     .S_AXI_WSTRB   (axi_xif.s_axi_wstrb),
+
+     // Write Response
+     .S_AXI_BVALID  (axi_xif.s_axi_bvalid),
+     .S_AXI_BREADY  (axi_xif.s_axi_bready),
+     .S_AXI_BRESP   (axi_xif.s_axi_bresp),
+
+     // Read Address
+     .S_AXI_ARVALID (axi_xif.s_axi_arvalid),
+     .S_AXI_ARREADY (axi_xif.s_axi_arready),
+     .S_AXI_ARADDR  (axi_xif.s_axi_araddr),
+     .S_AXI_ARPROT  (axi_xif.s_axi_arprot),
+
+     // Read Data
+     .S_AXI_RVALID  (axi_xif.s_axi_rvalid),
+     .S_AXI_RREADY  (axi_xif.s_axi_rready),
+     .S_AXI_RDATA   (axi_xif.s_axi_rdata),
+     .S_AXI_RRESP   (axi_xif.s_axi_rresp),
+
+     // Master AXI-Lite Write Address Channel
+     .M_AXI_AWADDR  (axi_xif.m_axi_awaddr),
+     .M_AXI_AWPROT  (axi_xif.m_axi_awprot),
+     .M_AXI_AWVALID (axi_xif.m_axi_awvalid),
+     .M_AXI_AWREADY (axi_xif.m_axi_awready),
+
+     // Write Data
+     .M_AXI_WDATA   (axi_xif.m_axi_wdata),
+     .M_AXI_WSTRB   (axi_xif.m_axi_wstrb),
+     .M_AXI_WVALID  (axi_xif.m_axi_wvalid),
+     .M_AXI_WREADY  (axi_xif.m_axi_wready),
+
+     // Write Response
+     .M_AXI_BRESP   (axi_xif.m_axi_bresp),
+     .M_AXI_BVALID  (axi_xif.m_axi_bvalid),
+     .M_AXI_BREADY  (axi_xif.m_axi_bready),
+
+     // Read Address
+     .M_AXI_ARADDR  (axi_xif.m_axi_araddr),
+     .M_AXI_ARPROT  (axi_xif.m_axi_arprot),
+     .M_AXI_ARVALID (axi_xif.m_axi_arvalid),
+     .M_AXI_ARREADY (axi_xif.m_axi_arready),
+
+     // Read Data
+     .M_AXI_RDATA   (axi_xif.m_axi_rdata),
+     .M_AXI_RRESP   (axi_xif.m_axi_rresp),
+     .M_AXI_RVALID  (axi_xif.m_axi_rvalid),
+     .M_AXI_RREADY  (axi_xif.m_axi_rready)
+  );
+
+  
+
+
+
+  generate
+  for (i = 0; i < 8; i++) begin : SLAVE
+
+      axi_slave_mem slave (
+          .ACLK    (clk),
+          .ARESETN (rst_n),
+
+          .AWADDR  (axi_xif.m_axi_awaddr[i*32 +: 32]),
+          .AWVALID (axi_xif.m_axi_awvalid[i]),
+          .AWREADY (axi_xif.m_axi_awready[i]),
+
+          .WDATA   (axi_xif.m_axi_wdata[i*32 +: 32]),
+          .WVALID  (axi_xif.m_axi_wvalid[i]),
+          .WREADY  (axi_xif.m_axi_wready[i]),
+
+          .BVALID  (axi_xif.m_axi_bvalid[i]),
+          .BREADY  (axi_xif.m_axi_bready[i]),
+
+          .ARADDR  (axi_xif.m_axi_araddr[i*32 +: 32]),
+          .ARVALID (axi_xif.m_axi_arvalid[i]),
+          .ARREADY (axi_xif.m_axi_arready[i]),
+
+          .RDATA   (axi_xif.m_axi_rdata[i*32 +: 32]),
+          .RVALID  (axi_xif.m_axi_rvalid[i]),
+          .RREADY  (axi_xif.m_axi_rready[i])
+      );
+
+  end
+endgenerate
+
+  initial begin
+    `uvm_config_db(virtual axi_xbar_if)::set(uvm_root::get(), "*", "axi_xbar_if", axi_xif)
+    
+    run_test("test_name");
+    
+  end
+  
+  
+  
+  initial begin
+    clk = 0;
+    rst_n = 0;
+    
+    @repeat (2) (negedge clk);
+    
+    rst_n = 1;
+  end
+  
+  forever begin
+    #5ns clk = ~clk;
+  end
+  
+  
+endmodule
