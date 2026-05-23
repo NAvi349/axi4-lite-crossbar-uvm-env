53e53b50f7327edcd5473b0702951b3e74f27344
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-05-23 19:11:50 +0530

    Create axi_xbar_master_monitor.sv
    This contains the master input/ slave output monitor

---
+++ tb/axi_xbar_master_monitor.sv
@@ -0,0 +1,14 @@
+/*
+Description: This is the monitor for master input/slave output.
+There are two monitors here for both input (master) and output (slave).
+
+
+
+*/
+
+
+class axi_xbar_master_monitor extends uvm_monitor;
+  
+  
+  
+endclass
