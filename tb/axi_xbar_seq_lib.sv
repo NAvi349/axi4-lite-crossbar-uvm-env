17ff1c1a3e1270194eacd2e0b28c6de822986737
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-05-23 19:14:15 +0530

    Create axi_xbar_seq_lib.sv
    This is the sequence library

---
+++ tb/axi_xbar_seq_lib.sv
@@ -0,0 +1,55 @@
+
+// Sequence used for master input/slave output
+class axi_xbar_master_sequence extends uvm_sequence();
+  
+  axi_xbar_item axi_m_tx;
+  
+ `uvm_object_utils(axi_xbar_master_sequence)
+ 
+  function new(string name = "axi_xbar_master_sequence");
+    super.new(name);
+  endfunction: new
+  
+  
+  task body();
+   
+    axi_m_tx = axi_xbar_item::type_id::create("axi_m_tx");
+    
+    // send addr + data in single sequence item
+    start_item(axi_m_tx);  // request grant
+               
+    axi_m_tx.randomize();
+    
+    finish_item(axi_m_tx);
+    
+  endtask
+  
+  
+endclass: axi_xbar_master_sequence
+
+
+//Sequence used for slave input/master output
+class axi_xbar_slave_sequence extends uvm_sequence();
+  
+  axi_xbar_item axi_s_tx;
+  
+ `uvm_object_utils(axi_xbar_slave_sequence)
+  
+  function new(string name = "axi_xbar_slave_sequence");
+    super.new(name);
+  endfunction: new
+  
+  
+  task body();
+    
+    // send ready for valid 
+    
+    start_item(axi_s_tx);
+    
+    axi_s_tx.randomize();
+    
+    finish_item(axi_s_tx);
+    
+  endtask
+  
+endclass: axi_xbar_slave_sequence
