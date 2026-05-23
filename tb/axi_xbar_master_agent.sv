5316708fae19cba543dd8271836366281382964d
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-05-23 19:10:51 +0530

    Create axi_xbar_master_agent.sv
    this contains the master agent

---
+++ tb/axi_xbar_master_agent.sv
@@ -0,0 +1,29 @@
+class axi_xbar_master_agent extends uvm_agent;
+ `uvm_component_utils(axi_xbar_master_agent)
+  
+  axi_xbar_master_driver axi_x_m_drvr;
+  axi_xbar_master_monitor axi_x_m_mon;
+  axi_xbar_master_sequencer axi_x_m_sqr;
+  
+  function new(string name = "axi_xbar_master_agent", uvm_component parent);
+    super.new(name, parent);
+
+  endfunction
+  
+  function void build_phase (uvm_phase phase);
+    super.build_phase(phase);
+    axi_x_m_drvr = axi_xbar_master_driver::type_id::create("axi_x_m_drvr", this);
+    axi_x_m_mon  = axi_xbar_master_monitor::type_id::create("axi_x_m_mon", this);
+    axi_x_m_sqr  = axi_xbar_master_sequencer::type_id::create("axi_x_m_sqr", this);
+  endfunction
+  
+  function void connect_phase (uvm_phase phase);
+    axi_x_m_drvr.seq_item_port.connect(axi_x_m_sqr.seq_item_export);
+        
+  endfunction
+  
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+  endtask
+  
+endclass
