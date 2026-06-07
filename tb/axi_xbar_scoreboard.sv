c91b85a577b526280e0cb24661af421544b11a96
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-06-07 10:46:56 +0530

    Update axi_xbar_scoreboard.sv
    updated scoreboard for compilation clean

--- tb/axi_xbar_scoreboard.sv
+++ tb/axi_xbar_scoreboard.sv
@@ -1,5 +1,7 @@
 /* Description: 
+The scoreboard will have four ports.
 
+It will receive the transactions from both master and slave side monitors.
 
 
 */
@@ -7,14 +9,21 @@
 class axi_xbar_scoreboard extends uvm_scoreboard;
  `uvm_component_utils(axi_xbar_scoreboard)
   
-  
+  uvm_tlm_analysis_fifo #(axi_xbar_item) m_tx_in_fifo;
+  uvm_tlm_analysis_fifo #(axi_xbar_item) m_tx_out_fifo;
+  uvm_tlm_analysis_fifo #(axi_xbar_item) s_tx_in_fifo;
+  uvm_tlm_analysis_fifo #(axi_xbar_item) s_tx_out_fifo;
   
   function new(string name = "axi_xbar_scoreboard", uvm_component parent);
     super.new(name, parent);
   endfunction
   
   function void build_phase(uvm_phase phase);
-    super.build_phase(phase);    
+    super.build_phase(phase);
+    m_tx_in_fifo = new("m_tx_in_fifo", this);
+    m_tx_out_fifo = new("m_tx_out_fifo", this);
+    s_tx_in_fifo = new("s_tx_in_fifo", this);
+    s_tx_out_fifo = new("s_tx_out_fifo", this);
   endfunction: build_phase
   
   task run_phase(uvm_phase phase);
