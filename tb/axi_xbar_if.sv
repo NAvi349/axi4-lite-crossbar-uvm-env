3883744d23efb64c767f750e1035425febef896c
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-06-07 10:44:22 +0530

    Update axi_xbar_if.sv
    commented out clocking blocks for now will implement later

--- tb/axi_xbar_if.sv
+++ tb/axi_xbar_if.sv
@@ -41,16 +41,58 @@ interface axi_xbar_if(input clock, input rst_n);
   logic [  7:0]   m_axi_rvalid;
   logic [  7:0]   m_axi_rready;
   
+  /*
   // M_driver
-  clocking cb_m();
+  clocking cb_m_d @(posedge clock);
+    //default input #setup_time output #hold_time;
+  
+    output s_axi_awvalid;
+    output s_axi_awprot;
+    output s_axi_wvalid;
+    output s_axi_wdata;
+    output s_axi_wstrb;
+    output s_axi_bready;
+    output s_axi_arvalid;
+    output s_axi_araddr;
+    output s_axi_arprot;
+    output s_axi_rready;
+     
+    input s_axi_awready;
+    input s_axi_wready;
+    input s_axi_bvalid;
+    input s_axi_bresp;
+    input s_axi_arready;
+    input s_axi_rvalid;
+    input s_axi_rdata;
+    input s_axi_rresp;
     
   endclocking
   
-  // S_driver
+  // slave input driver
     
-  clocking cb_s();
-      
+  clocking cb_s_d @(posedge clock);
+    input m_axi_awready;
+    input m_axi_wready;
+    input m_axi_bresp;
+    input m_axi_bvalid;
+    input m_axi_arready;
+    input m_axi_rdata;
+    input m_axi_rresp;
+    
+    
+    output m_axi_awaddr;
+    output m_axi_awprot;
+    output m_axi_awvalid;
+    output m_axi_wdata;
+    output m_axi_wstrb;
+    output m_axi_wvalid;
+    output m_axi_bready;
+    output m_axi_araddr;
+    output m_axi_arprot;
+    output m_axi_arvalid;
+    output m_axi_rready;
   endclocking
+  */
   
   
 endinterface
