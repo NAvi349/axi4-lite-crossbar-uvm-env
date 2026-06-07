a64e2a42b7ced8240f869b5e53be8ee84325ed6f
Navinkumar K <66086031+NAvi349@users.noreply.github.com>
2026-06-07 10:43:39 +0530

    Update axi_xbar_env.sv
    Updated with agent creation

--- tb/axi_xbar_env.sv
+++ tb/axi_xbar_env.sv
@@ -1,7 +1,5 @@
 /*
 Description: AXI Cross environment. This is a multi-agent env as it can be driven from both sides.
-
-
 */
 
 
@@ -13,17 +11,27 @@ class axi_xbar_env extends uvm_env;
   axi_xbar_scoreboard axi_x_scbd;  // scoreboard
   
   
-  function new(string name = "axi_bar_env", uvm_component parent);
+  function new(string name = "axi_xbar_env", uvm_component parent);
     super.new(name, parent);
   endfunction
   
   function void build_phase(uvm_phase phase);
-    super.build_phase(phase);    
+    super.build_phase(phase);
+    axi_m_agt = axi_xbar_master_agent::type_id::create("axi_m_agt", this);
+    axi_s_agt = axi_xbar_slave_agent::type_id::create("axi_s_agt", this);
+    axi_x_scbd = axi_xbar_scoreboard::type_id::create("axi_x_scbd", this);
   endfunction
   
   function void connect_phase(uvm_phase phase);
     // connect analysis ports from master monitors to scoreboard
     // connect analysis ports from slave monitors to scoreboard
+    
+    axi_m_agt.axi_x_m_mon.dut_m_tx_in.connect(axi_x_scbd.m_tx_in_fifo.analysis_export);
+    axi_m_agt.axi_x_m_mon.dut_m_tx_out.connect(axi_x_scbd.m_tx_out_fifo.analysis_export);
+    axi_s_agt.axi_x_s_mon.dut_slave_tx_in.connect(axi_x_scbd.s_tx_in_fifo.analysis_export);
+    axi_s_agt.axi_x_s_mon.dut_slave_tx_out.connect(axi_x_scbd.s_tx_out_fifo.analysis_export);
+    
+  
   endfunction
   
   
